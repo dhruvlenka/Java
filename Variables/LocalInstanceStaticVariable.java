a69dd96b71ea82334a0339137a2f9c3266aed024
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-14 14:29:03 +0530

    Create LocalInstanceStaticVariable.java

---
+++ Variables/LocalInstanceStaticVariable.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Method;
+
+public class LocalInstOrStatic {
+    static int staticVariable = 21;
+    /* Static variable is independent,
+       Anyone can access static variable inside the body,
+       And static variable don't need any object.
+    */
+
+    int InstanceVariable = 18;
+    void method(){
+        int localVariable = 15;
+    // We can't access Local variable outside any method
+    }
+    public static void main(String[] args) {
+        LocalInstOrStatic instVar = new LocalInstOrStatic();
+        System.out.println(instVar.InstanceVariable); // For instance variable
+        System.out.println(LocalInstOrStatic.staticVariable);
+    }
+}
