5af4e8abbd11eec3ec564b28d0b9472b68b38274
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-14 14:25:54 +0530

    Update LocalVariable.java

--- Variables/LocalVariable.java
+++ Variables/LocalVariable.java
@@ -1,1 +1,10 @@
+// Program on Local Variable
+public class LocalVariable {
+    public static void main(String[] args) {
+        int x = 21;
+        int y = 19;
+        int z = x+y;
+        System.out.println("Adding X and Y = " + z);
+    }
+}
 
