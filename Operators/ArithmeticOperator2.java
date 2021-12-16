2040ad6828e71f27df892b3653d236fa4f10def1
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-16 12:38:31 +0530

    Create ArithmeticOperator2.java

---
+++ Operators/ArithmeticOperator2.java
@@ -0,0 +1,13 @@
+public class ArithmeticOperator {
+  public static void main(String[] args)
+  {
+    int a = 2 + 4;
+    int b = a - 2;
+    int c = b * 4;
+    int d = c / 4;
+    System.out.println("Addition of a = " + a);
+    System.out.println("Substraction of b = " + b);
+    System.out.println("Multiplication of c = " + c);
+    System.out.println("Modulos of d = " + d);
+  }
+}
