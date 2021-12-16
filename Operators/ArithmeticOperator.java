49c78a1bfaaba8124a0cd82b42ee208f29e4a3bc
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-16 12:37:33 +0530

    Create ArithmeticOperator.java

---
+++ Operators/ArithmeticOperator.java
@@ -0,0 +1,17 @@
+public class ArithmeticOperator {
+     public static void main(String[] args) 
+     {
+       int a = 9;
+       int b = 2;
+       int add = a+b;
+       int subs = a-b;
+       int multi = a*b;
+       int divi = a/b;
+       int modulo = a%b;
+       System.out.println("Addition of 9 + 2 = " + add);    
+       System.out.println("Substraction of 9 - 2 = " + subs);
+       System.out.println("Multiplication of 9 * 2 = " + multi);
+       System.out.println("Division of 9 / 2 = " + divi);
+       System.out.println("Modulus of 9 % 2 = " + modulo);
+    }
+}
