07fd408c9b9274b6e7559a4d7c426edfc927470c
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-20 00:00:44 +0530

    Create IncOrDec3.java

---
+++ Operators/IncOrDec3.java
@@ -0,0 +1,12 @@
+public class IncOrDec3 {
+    public static void main(String[] args) {
+        int a = 10; 
+        a--; 
+        int b=10;
+        ++b; 
+        System.out.println(a); //9
+        System.out.println(b); //11
+        System.out.println(a++ + ++a); //20
+        System.out.println(b++ + b++); //23
+    }
+}
