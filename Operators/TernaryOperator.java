abfa394ce616e2f5da3014fa990e081734d572f2
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-20 23:41:53 +0530

    Create TernaryOperator.java

---
+++ Operators/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator {
+    public static void main(String[] args) 
+    {
+        int dhruvage = 21;
+        int ayushage = 18;
+        int minAge = (dhruvage>ayushage ? dhruvage : ayushage);
+        System.out.println(minAge);
+    }
+}
