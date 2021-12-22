15937dbef294d055b63cf23cd6ba93eb4905e4fd
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-22 21:50:49 +0530

    Create OROperator.java

---
+++ Operators/OROperator.java
@@ -0,0 +1,12 @@
+public class OROperator {
+    public static void main(String[] args) {
+        int x = 18;
+        int y = 14;
+        /*
+        18 in Binary = 0010
+        14 in Binary = 1110
+        0010 | 1110 = 1110 = 30 will be printed
+        */
+        System.out.println("X | Y = "+ (x|y));
+    }
+}
