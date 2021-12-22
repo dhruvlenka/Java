fcccfe4fc6e0c3f667d4508bd86bd17a07e7363a
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-22 20:43:10 +0530

    Create LogicalANDOperator.java

---
+++ Operators/LogicalANDOperator.java
@@ -0,0 +1,9 @@
+public class ANDOperator {
+    public static void main(String[] args) {
+        int a = 21;
+        int b = 18;
+        int c = 19;
+        System.out.println(a<b && a<c); //false
+        System.out.println(a<b & a<c); //false
+    }
+}
