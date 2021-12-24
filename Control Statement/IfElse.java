66f0d8ed9cbcc9446c3df4ce82ac9ecaeb31a87a
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-24 12:40:57 +0530

    Create IfElse.java

---
+++ Control Statement/IfElse.java
@@ -0,0 +1,10 @@
+// Writing a program upon IF-Else 
+public class IFElse {
+    public static void main(String[] args) {
+        int age = 8;
+        if (age >=18) 
+        System.out.println("You can watch this video");
+         else 
+        System.out.println("You can't watch this video");
+    }
+}
