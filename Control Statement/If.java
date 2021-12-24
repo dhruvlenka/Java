9e411e9ee16226893f5728d660367b7a97c207a4
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-24 12:27:52 +0530

    Update and rename IF.java to If.java

---
+++ Control Statement/If.java
@@ -0,0 +1,7 @@
+public class IfProgram {
+    public static void main(String[] args) {
+        int donationAmount = 150;
+        if(donationAmount >= 100)
+            System.out.println("You can Donate");
+    }
+}
