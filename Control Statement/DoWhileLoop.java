78a2383dd8b6a8be99f172a36ba7f12057215339
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-27 14:01:25 +0530

    Create DoWhileLoop.java

---
+++ Control Statement/DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        do
+        {
+          System.out.println("Hello");// This will Infinite Time Hello
+        }
+        while(10<20);
+      }
+}
+        
