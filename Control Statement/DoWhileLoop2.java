77d0fc6a3d4437f75cca290b2800635ca20d49ac
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-27 14:05:51 +0530

    Create DoWhileLoop2.java

---
+++ Control Statement/DoWhileLoop2.java
@@ -0,0 +1,9 @@
+public class DoWhileLoop2 {
+    public static void main(String[] args) {
+      do {
+        System.out.println("Hello");
+         }
+      while (10<20);
+        System.out.println("Hi");
+    }
+}
