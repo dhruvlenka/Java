508f118dccf891a876eadee317c362657193f532
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-27 14:15:15 +0530

    Create DoWhileLoop4.java

---
+++ Control Statement/DoWhileLoop4.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop2 {
+    public static void main(String[] args) {
+        final int a = 10;
+        int b = 20;
+        do {
+            System.out.println("True");
+        }
+        while (a>20 && b <= 20);
+        System.out.println("False");
+    }
+}
