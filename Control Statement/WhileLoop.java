ac27483dfce104f4263554b7558480e616263240
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-27 12:50:23 +0530

    Create WhileLoop.java

---
+++ Control Statement/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+    public static void main(String[] args) 
+    {
+        int a = 10;
+        int b = 20;
+        while (a<b) 
+     {
+        System.out.println("Hello Git"); 
+        // This will print "Hello Git" infinite times.
+     }     
+    }
+}
