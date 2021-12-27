24ecf86e1d609e5189ef5049f90362a36baad1f0
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-27 14:10:46 +0530

    Update DoWhileLoop3.java

--- Control Statement/DoWhileLoop3.java
+++ Control Statement/DoWhileLoop3.java
@@ -4,6 +4,11 @@ public class DoWhileLoop3 {
           System.out.println("Hello");
         }
        while(10>20);
+          // This condition is false, This will print Hello,Hi!
          System.out.println("Hi");
+          /*
+           If condition will be true, then Hello will print infinite times.
+           Curly brackets doesn't matter for 1 output.
+          */
      } 
 }
