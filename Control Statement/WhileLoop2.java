fd607795ead6ba78c91e128a163c3af77a27f931
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-27 13:01:33 +0530

    Create WhileLoop2.java

---
+++ Control Statement/WhileLoop2.java
@@ -0,0 +1,20 @@
+public class WhileLoop2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        final int c = 30;
+        while(b<c)
+        {
+            System.out.println("Welcome To Java");
+            /*
+            If condition True,
+            "Welcome To Java" will be print infinite times
+            */
+        }
+            /*
+            If condition false,
+            "You are learning" will be print
+            */
+            System.out.println("You are learning");
+    }
+}
