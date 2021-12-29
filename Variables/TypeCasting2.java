238d22a249dd7eada29bc85b25acaf89bb4253a3
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2021-12-29 12:13:33 +0530

    Create TypeCasting2.java

---
+++ Variables/TypeCasting2.java
@@ -0,0 +1,13 @@
+public class TypeCasting2 {
+    public static void main(String[] args) {
+        int a = 257;
+        byte b = (byte) (a); 
+        /*
+        The maximum value can be stored in a byte is 0 through 255 = 256
+        We write in int a = 257, which exceed the byte value,
+        If we convert int to byte then we will get 1 reminder of 257-256 is 1
+        */
+        System.out.println(b);
+
+    }
+}
