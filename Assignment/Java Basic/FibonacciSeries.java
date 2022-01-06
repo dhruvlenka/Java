770a18754d0ca500361122f9502833f4f2f2a721
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-06 12:57:21 +0530

    Update FibonacciSeries.java

--- Assignment/Java Basic/FibonacciSeries.java
+++ Assignment/Java Basic/FibonacciSeries.java
@@ -1,1 +1,19 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int number1 = 0;
+        int number2 = 1;
+        int number3, count = 20;
+        System.out.print(number1 + " " + number2);
+
+        for(int i=2; i<count; i++){
+            number3 = number1 + number2;
+            System.out.print(" "+number3);
+
+            number1 = number2;
+            number2 = number3;
+    
+        }
+       
+    }
+}
 
