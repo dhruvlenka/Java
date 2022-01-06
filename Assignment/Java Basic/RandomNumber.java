3c14e9ba60f848089d58a4c8a7b9729566212934
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-06 16:15:13 +0530

    Create RandomNumber.java

---
+++ Assignment/Java Basic/RandomNumber.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+public class RandomNumber {
+    public static void main(String[] args) {
+        
+        System.out.println("Random Number 1 = " + Math.random());
+        System.out.println("Random Number 2 = " + Math.random());
+        System.out.println("Random Number 3 = " + Math.random());
+        System.out.println("Random Number 4 = " + Math.random());
+        System.out.println("Random Number 5 = " + Math.random());
+    }
+}
