522106bae1629fd35be1c7a52e054470cb7376f8
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-07 09:25:10 +0530

    Create ASCII.java

---
+++ Assignment/Java Basic/ASCII.java
@@ -0,0 +1,27 @@
+public class ASCII {
+    public static void main(String[] args) {
+        /*
+        int ch1 = 'D';
+        int ch2 = 'h';
+        int ch3 = 'r';
+        int ch4 = 'u';
+        int ch5 = 'v';
+        System.out.println(ch1);
+        System.out.println(ch2);
+        System.out.println(ch3);
+        System.out.println(ch4);
+        System.out.println(ch5);
+        */
+        
+        char c1 = 'D';
+        char c2=  'L';
+
+        int number1 = c1;
+        int number2 = c2;
+
+        System.out.println(number1);
+        System.out.println(number2);
+
+
+    }
+}
