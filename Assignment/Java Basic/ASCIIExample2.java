ce0803e9f2a468136b3b003077abf16e9a88cfd5
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-07 13:30:43 +0530

    Create ASCIIExample2.java

---
+++ Assignment/Java Basic/ASCIIExample2.java
@@ -0,0 +1,11 @@
+public class ASCIIExample2 {
+  public static void main(String[] args) {
+      
+    // for(char i = 'a'; i<= 'z'; i++)
+    // System.out.println("ASCII Value of " + i + " is " + (int)i);
+
+       for(char j = 'A'; j<= 'Z'; j++){
+       System.out.println("ASCII Value of " + j + " is " + (int)j);
+       }
+  }
+}
