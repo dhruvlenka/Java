3a5cc6e308a349916914a81f2023613bf6cb427a
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-20 17:02:29 +0530

    Create PredefindFunction.java

---
+++ Function/PredefindFunction.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+//Program on Predefind function or method
+public class Function {
+    public static void main(String[] args) {
+        System.out.println("Maximum Value is = " + Math.max(21, 19));
+        System.out.println("Minimum Value is = " + Math.min(21, 19));
+    }
+}
