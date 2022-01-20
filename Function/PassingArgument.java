7b2bd5e93ecd05ddfbdd350c6ca2b8dff609f864
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-20 17:05:25 +0530

    Create PassingArgument.java

---
+++ Function/PassingArgument.java
@@ -0,0 +1,11 @@
+// Passing Argument
+public class Function3 {
+    public static void main(String[] args) {
+        int ans = addition(21, 18);
+    }
+    static int addition(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
+
