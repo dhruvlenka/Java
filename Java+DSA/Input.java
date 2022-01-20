29c40ea3c3e28c6b49e2626b8ca7d4dce35dc095
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-20 17:00:58 +0530

    Delete Java+DSA directory

--- Java+DSA/Input.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Input2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter Your Age = ");
-        System.out.println(input.nextInt());
-    }
-    
-}
