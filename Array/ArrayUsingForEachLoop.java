d8136fd7543090f071acad63dac812ae5a8fb76b
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-01-21 14:19:53 +0530

    Create ArrayUsingForEachLoop.java

---
+++ Array/ArrayUsingForEachLoop.java
@@ -0,0 +1,9 @@
+// Writing a program for accesing element through For each loop
+public class ArrayUsingForEachLoop {
+    public static void main(String[] args) {
+        int[] age = {21,18,19}; // declaration, instantiation, initialization
+        for(int a : age)       // Priting array using for each loop
+        System.out.println(a); 
+
+    }
+}
