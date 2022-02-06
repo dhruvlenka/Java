9b710bc091555d7d7e2d1d7f1c9cf989c65941c9
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-02-06 10:08:29 +0530

    Create ArrayListInput.java

---
+++ Array/ArrayListInput.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayListExamples {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Numbers = ");
+        //ArrayList
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < 5; i++) {
+            list.add(input.nextInt());
+        }
+       System.out.println(list);
+    }
+}
