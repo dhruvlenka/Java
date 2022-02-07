e56c690591486ec5a2d40f200cefba02af713dc8
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-02-07 21:41:02 +0530

    Create Concat.java

---
+++ String/Concat.java
@@ -0,0 +1,9 @@
+package com.dhruv;
+
+public class Concat {
+    public static void main(String[] args) {
+        String s = "The Dhruv";
+        s = s.concat(" Lenka");
+        System.out.println(s);
+    }
+}
