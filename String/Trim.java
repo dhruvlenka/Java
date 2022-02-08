42bdef357713c6a41bb774d6e13e057db0891474
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-02-08 19:29:34 +0530

    Create Trim.java

---
+++ String/Trim.java
@@ -0,0 +1,9 @@
+package com.dhruv;
+
+public class Trim {
+    public static void main(String[] args) {
+        String charActer = " Lucifer ";
+        System.out.println(charActer+ "MorningStar"); // Without trim
+        System.out.println(charActer.trim()+ "MorningStar"); // With trim
+    }
+}
