fe32c763240686a87cbd3fcafc225f6facd77e5a
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-03-04 22:26:15 +0530

    Update PalindromeNumber.java

--- Assignment/Java Basic/PalindromeNumber.java
+++ Assignment/Java Basic/PalindromeNumber.java
@@ -1,24 +1,20 @@
 import java.util.Scanner;
-public class PalindromeNumberInput {
+public class PalindromeNumber {
     public static void main(String[] args) {
-        int y, z = 0, rem;  // variable
+        int temp, rem, count = 0;
+        Scanner input = new Scanner(System.in); //For Input
+        System.out.print("Enter a Number = ");
+        int num = input.nextInt();
+        temp = num; //Value Storing in temp var for practical basics
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a Numeber = ");
-        int x = input.nextInt(); //Input 121
-        y = x; // y = 121
-      
-        //Loop
-        while(x>0)  // 121 > 0 = True  // 12 > 0 = TRUE // 1 > 0 = TRUE
-        {
-            rem = x % 10; // For Reminder, 121 % 10 = 1, rem = 1, rem = 12%10 = 2, rem = 1 % 10 = 1 
-            z = (z * 10) + rem; // (0 * 1) + 1 = 1, (1*10) + 2 = 12, (12*10) + 1 = 121
-            x = x / 10;  // n = 12, n = 1, n = 0; 
+        while (num > 0){
+            rem = num % 10;
+            count = count * 10 + rem;
+            num = num / 10;
         }
-        if (y == z) {
-            System.out.println(y + " Palindrome Numeber");
-            } else {
-                System.out.println(" Not a Palindrome Number");
+        if(temp == count){
+            System.out.print(temp + " is Palindrome Number");
+        } else {
+            System.out.print(temp + " is not a Palindrome Number");
         }
-    }
-}
+  }
