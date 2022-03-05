f6f298f811900a7f5c396fc88afb8e897613536f
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-03-05 15:57:52 +0530

    Create NextMultiplication.java

---
+++ Assignment/Java Basic/NextMultiplication.java
@@ -0,0 +1,10 @@
+//Write a java program to return  a number of next multiplication of 10.
+public class NextMultiplicatin {
+    public static void main(String[] args) {
+        int num = 133;
+        int sol = ((num/10)+1)*10;
+        System.out.println(sol);
+
+    }
+    
+}
