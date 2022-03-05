3db1e44939f35d58eb189eb611b16efaddb05ad1
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-03-05 13:51:54 +0530

    Create Reverse.java

---
+++ Assignment/Java Basic/Reverse.java
@@ -0,0 +1,15 @@
+public class Reverse {
+  public static void main(Stringp[] args) [
+    int num = 390146;
+    int rev  = 0;
+     
+    while(num>0) {
+      int rem = num % 10;
+      number /= 10;
+      rev = rev * 10 + rem;
+      System.out.println(rem);
+      }
+    }
+  }
+    
+                          
