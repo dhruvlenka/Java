bab438514bed0664c1c9a9028dbe121068350d79
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-03-05 15:59:16 +0530

    Create NextMultiplication2.java

---
+++ Assignment/Java Basic/NextMultiplication2.java
@@ -0,0 +1,10 @@
+//Write a java program to return a number of next multiplication of 100.
+public class NextMultiplicatin {
+    public static void main(String[] args) {
+        int num = 233;
+        int sol = ((num/100)+1)*100;
+        System.out.println(sol);
+
+    }
+    
+}
