d1d864ea5403d9d62f846e3c71992de47a15ee79
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-03-11 13:27:34 +0530

    Create SumOfArray.java

---
+++ Array/Assignment/SumOfArray.java
@@ -0,0 +1,12 @@
+public class SumOfArray {
+    public static void main(String[] args) {
+        int[] arr = {2,4,5,6,7,8};
+        int sum = 0;
+        for (int i : arr) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+
+    }
+}
