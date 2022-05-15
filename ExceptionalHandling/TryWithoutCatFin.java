5f0043a36c73c8674d722c44a80443ebaae23bed
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-05-15 07:25:23 +0530

    Create TryWithoutCatFin.java

---
+++ ExceptionalHandling/TryWithoutCatFin.java
@@ -0,0 +1,9 @@
+//Try without cat and finally.
+public class Try {
+    public static void main(String[] args) {
+        try {
+            int marks = 50/0;
+            System.out.println(marks);
+        }
+    }
+}
