51b860c304c3075dc83bb1722ba212cb7efd995c
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-05-17 11:11:07 +0530

    Create Throw.java

---
+++ ExceptionalHandling/Throw.java
@@ -0,0 +1,13 @@
+public class Throw {
+    public static void voteAge(int age){
+        if (age < 18){
+            throw new ArithmeticException("Person is not eligible for vote");
+        } else {
+            System.out.println("Person is eligible for vote.");
+        }
+    }
+    public static void main(String[] args) {
+         voteAge(18);
+        System.out.println("You can vote, Thanks");
+    }
+}
