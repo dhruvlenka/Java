8486569900f3bc5b6cd6370f0c3079361c951f3d
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-05-30 12:21:39 +0530

    Create Throws.java

---
+++ OOPS/Throws.java
@@ -0,0 +1,17 @@
+package com.dhruvlenka;
+
+public class Throws {
+    int division(int a, int b) throws ArithmeticException  {
+        int aE = a/b;
+        return aE;
+    }
+    public static void main(String[] args) {
+     Throws thr = new Throws();
+     try{
+         System.out.println(thr.division(15,0));
+     }
+     catch (ArithmeticException AE){
+         System.out.println("You shouldn't divide number by zero.");
+     }
+    }
+}
