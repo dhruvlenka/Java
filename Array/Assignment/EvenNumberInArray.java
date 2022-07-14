9f596811965783e9fd956be0405773053c4912ed
DhruvLenka <72161580+thedhruvlenka@users.noreply.github.com>
2022-07-14 12:54:10 +0530

    Update EvenNumberInArray.java

--- Array/Assignment/EvenNumberInArray.java
+++ Array/Assignment/EvenNumberInArray.java
@@ -1,12 +1,22 @@
 public class EvenNumberInArray {
     public static void main(String[] args) {
-
-        int[] even = {1,2,3,4,5,6,7,8,9,10,12};
-        System.out.print("Even number elements in given Array = ");
-        for (int i = 1; i < even.length; i += 2) {
-            System.out.print(even[i] + " ");
-
-
+        int[] even = {2,4,6,1,3,5,7,9,8,10,12};
+       /*
+       for (int i = 0; i<even.length;i++){
+            if (even[i] % 2 == 0){
+                System.out.println(even[i]+ " EVEN ");
+            }
+            else {
+                System.out.println(even[i]+ " ODD ");
+            }
+        }
+        */
+        for (int j : even) {
+            if (j % 2 == 0) {
+                System.out.println(j + " EVEN ");
+            } else {
+                System.out.println(j + " ODD ");
+            }
         }
     }
 }
